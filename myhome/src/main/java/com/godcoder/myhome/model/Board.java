86f2e203c7ab5eab6390ec4088d51dbb83199dd3
ohMySunset <znfnznfnakqj@naver.com>
2021-04-20 02:01:58 +0900

    Update : validation 어노테이션 활용 - 20210420

--- myhome/src/main/java/com/godcoder/myhome/model/Board.java
+++ myhome/src/main/java/com/godcoder/myhome/model/Board.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -14,6 +16,9 @@ public class Board { // 게시글 데이터를 담을 빈즈객체
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    //validation 어노테이션을 활용한 유효성 검사 (간편)
+    @NotNull
+    @Size(min=2, max=30, message = "제목은 2자 이상 30자 이하입니다.")
     private String title;
     private String content;
 }
