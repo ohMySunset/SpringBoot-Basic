3d71eb627ae0f82d04c63c3b7a3c479e76d1808b
ohMySunset <znfnznfnakqj@naver.com>
2021-04-30 02:04:16 +0900

    Update : @ManyToOne 관계 처리하기 - 20210430

--- myhome/src/main/java/com/godcoder/myhome/controller/BoardController.java
+++ myhome/src/main/java/com/godcoder/myhome/controller/BoardController.java
@@ -2,12 +2,15 @@ package com.godcoder.myhome.controller;
 
 import com.godcoder.myhome.model.Board;
 import com.godcoder.myhome.repository.BoardRepository;
+import com.godcoder.myhome.service.BoardService;
 import com.godcoder.myhome.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -24,6 +27,8 @@ public class BoardController {
     private BoardRepository boardRepository;
     @Autowired
     private BoardValidator boardValidator;
+    @Autowired
+    private BoardService boardService
 
     @GetMapping("/list")
     public String list(Model model, @PageableDefault(size = 2) Pageable pageable,
@@ -59,17 +64,22 @@ public class BoardController {
     }
 
     @PostMapping("/form")
-    public String greetingSubmit(@Valid Board board, BindingResult br){
+    public String postForm(@Valid Board board, BindingResult br, Authentication authentication){
 
         // 유효성 검사 (자바 코드로)
         boardValidator.validate(board, br);
-
         // 유효성 검사 (어노테이션 활용)
         if(br.hasErrors()){
             return "board/form";
         }
+        // 현재 사용자의 유저아이디 받아서 저장
+        //Authentication a = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        boardService.save(username, board);
+
         // 게시물 저장 (이미 존재하는 경우 수정)
-        boardRepository.save(board);
+        //boardRepository.save(board);
+
         //board 데이터를 저장만하기 때문에 다시 조회된 값을 뿌려주기 위해서 redirect를 써준다.
         return "redirect:/board/list";
     }
--- myhome/src/main/java/com/godcoder/myhome/model/Board.java
+++ myhome/src/main/java/com/godcoder/myhome/model/Board.java
@@ -2,10 +2,7 @@ package com.godcoder.myhome.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -21,4 +18,10 @@ public class Board { // 게시글 데이터를 담을 빈즈객체
     @Size(min=2, max=30, message = "제목은 2자 이상 30자 이하입니다.")
     private String title;
     private String content;
+
+    // JPA를 이용하여 @ManyToOne 관계 설정하기
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
 }
